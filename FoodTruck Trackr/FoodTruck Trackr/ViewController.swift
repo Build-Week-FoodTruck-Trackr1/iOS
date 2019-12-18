13435c9a93aab4431256aa51e9c24c55b3ef8bdb
Joel Groomer <joelgroomer@gmail.com>
2019-12-17 20:35:03 -0800

    Initial project commit

---
+++ FoodTruck Trackr/FoodTruck Trackr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTruck Trackr
+//
+//  Created by Joel Groomer on 12/17/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
