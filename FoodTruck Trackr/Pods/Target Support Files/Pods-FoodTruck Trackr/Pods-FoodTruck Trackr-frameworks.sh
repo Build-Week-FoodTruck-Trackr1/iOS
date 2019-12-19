1b9801bcfe6a64868b48ea23de6d235240e9b08a
Joel Groomer <joelgroomer@gmail.com>
2019-12-18 19:14:57 -0800

    Added garland-view pod

--- FoodTruck Trackr/Pods/Target Support Files/Pods-FoodTruck Trackr/Pods-FoodTruck Trackr-frameworks.sh
+++ FoodTruck Trackr/Pods/Target Support Files/Pods-FoodTruck Trackr/Pods-FoodTruck Trackr-frameworks.sh
@@ -163,10 +163,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MapViewPlus/MapViewPlus.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/garland-view/garland_view.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MapViewPlus/MapViewPlus.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/garland-view/garland_view.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
