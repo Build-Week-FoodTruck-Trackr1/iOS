58bdf5397a8b3c7c76ed2d55596b9376802f720e
Jessie <jessie.griffin@icloud.com>
2019-12-18 23:54:24 -0500

    Adds MenuTableViewController and MenuItemTableViewCell

---
+++ FoodTruck Trackr/FoodTruck Trackr/MenuItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuItemTableViewCell.swift
+//  FoodTruck Trackr
+//
+//  Created by Jessie Ann Griffin on 12/18/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
