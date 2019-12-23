b7a427a5694325d7e8dc1295d1e22fb8eb7f8922
Jessie <jessie.griffin@icloud.com>
2019-12-22 21:55:01 -0500

    Adds Convenience Init for MenuItems

--- FoodTruck Trackr/FoodTruck Trackr/Model/MenuItem+Convenience.swift
+++ FoodTruck Trackr/FoodTruck Trackr/Model/MenuItem+Convenience.swift
@@ -10,5 +10,48 @@ import Foundation
 import CoreData
 
 extension MenuItem {
+    convenience init(
+        itemName: String,
+        itemPrice: Double,
+        itemPhoto: String?,
+        itemDescription: String,
+        customerRating: Int32?,
+        customerRatingAvg: Double?,
+        context: NSManagedObjectContext = CoreDataStack.shared.mainContext)
+    {
+        self.init(context: context)
+        self.itemName = itemName
+        self.itemPrice = itemPrice
+        self.itemPhoto = itemPhoto
+        self.itemDescription = itemDescription
+        if let customerRating = customerRating, let customerRatingAvg = customerRatingAvg
+        {
+            self.customerRating = customerRating
+            self.customerRatingAvg = customerRatingAvg
+        }
+    }
     
+    convenience init?(menuItemRepresentation: MenuItemRepresentation,
+                      context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(itemName: menuItemRepresentation.itemName,
+                  itemPrice: menuItemRepresentation.itemPrice,
+                  itemPhoto: menuItemRepresentation.itemPhoto,
+                  itemDescription: menuItemRepresentation.itemDescription,
+                  customerRating: menuItemRepresentation.customerRating,
+                  customerRatingAvg: menuItemRepresentation.customerRatingAvg,
+                  context: context)
+    }
+    
+    var menuItemRepresentation: MenuItemRepresentation? {
+        guard let itemName = itemName,
+            let itemDescription = itemDescription else { return nil }
+        
+        return MenuItemRepresentation(itemName: itemName,
+                                       itemPrice: itemPrice,
+                                       itemPhoto: itemPhoto,
+                                       itemDescription: itemDescription,
+                                       customerRating: customerRating,
+                                       customerRatingAvg: customerRatingAvg)
+    }
 }
---
+++ FoodTruck Trackr/FoodTruck Trackr/Model/MenuItemRepresentation.swift
@@ -0,0 +1,24 @@
+//
+//  MenuItemRepresentation.swift
+//  FoodTruck Trackr
+//
+//  Created by Jessie Ann Griffin on 12/22/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import Foundation
+
+//class Menu {
+//    var menuItems: [MenuItemRepresentation]?
+//}
+
+struct MenuItemRepresentation {
+    
+    var itemName: String
+    var itemPrice: Double
+    var itemPhoto: String?
+    var itemDescription: String
+    var customerRating: Int32?
+    var customerRatingAvg: Double?
+
+}
