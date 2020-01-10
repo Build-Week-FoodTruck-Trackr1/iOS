cd9c729de12a4b498bc922d3872c28a0aad960aa
Joel Groomer <joelgroomer@gmail.com>
2020-01-09 21:15:32 -0800

    Updated FoodTruckRepresentation to match new FoodTruck parameters

--- FoodTruck Trackr/FoodTruck Trackr/Model/FoodTruckRepresentation.swift
+++ FoodTruck Trackr/FoodTruck Trackr/Model/FoodTruckRepresentation.swift
@@ -13,8 +13,37 @@ struct FoodTruckRepresentation: Codable {
     var truckTitle: String
     var imageOfTruck: String?
     var cuisineType: String
-    var identifier: Int32
+    var truckID: Int32
     var customerRating: Int32?
     var customerRatingAvg: Double?
+    var currentLocation: String?
+    var currentDepartureTime: String?
+    var arrivalTime: String?
+    var location: String?
+    var departureTime: String?
+    
+    init(from foodTruck: FoodTruck) {
+        self.truckTitle = foodTruck.truckTitle ?? ""
+        self.imageOfTruck = foodTruck.imageOfTruck
+        self.cuisineType = foodTruck.cuisineType ?? ""
+        self.truckID = foodTruck.truckID
+        self.customerRating = foodTruck.customerRating
+        self.customerRatingAvg = foodTruck.customerRatingAvg
+        self.currentLocation = foodTruck.currentLocation
+        self.location = foodTruck.location                      // NEXT location the truck will be at
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm"
+        
+        if let cdt = foodTruck.currentDepartureTime {
+            self.currentDepartureTime = dateFormatter.string(from: cdt)
+        }
+        if let arrivalTime = foodTruck.arrivalTime {
+            self.arrivalTime = dateFormatter.string(from: arrivalTime)
+        }
+        if let departureTime = foodTruck.departureTime {
+            self.departureTime = dateFormatter.string(from: departureTime)
+        }
+    }
     
 }
