2d39109f5a3841f569d41a53e6d0a86d268ddd8f
Joel Groomer <joelgroomer@gmail.com>
2020-01-12 14:56:34 -0800

    Progress made on adding the map

--- FoodTruck Trackr/FoodTruck Trackr/View Controllers/ScheduleLocationViewController.swift
+++ FoodTruck Trackr/FoodTruck Trackr/View Controllers/ScheduleLocationViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapViewPlus
+import CoreLocation
 
 class ScheduleLocationViewController: UIViewController {
 
@@ -15,9 +16,15 @@ class ScheduleLocationViewController: UIViewController {
     
     var delegate: ScheduleViewController?
     var selectedFieldTag: Int?
+    private let locationManager = CLLocationManager()
+    private var locationServiceAuthorized = false
+    var currentLocation: CLLocation? = nil
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        startLocationServices()
     }
 
     @IBAction func saveTapped(_ sender: Any) {
@@ -25,5 +32,48 @@ class ScheduleLocationViewController: UIViewController {
     
     @IBAction func cancelTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ScheduleLocationViewController: CLLocationManagerDelegate {
+    func startLocationServices() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        let locationAuthorizationStatus = CLLocationManager.authorizationStatus()
+        
+        switch locationAuthorizationStatus {
+        case .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse, .authorizedAlways:
+            if CLLocationManager.locationServicesEnabled() {
+                locationServiceAuthorized = true
+                self.locationManager.startUpdatingLocation()
+                
+            }
+        case .restricted, .denied:
+            locationServiceAuthorized = false
+        @unknown default:
+            locationServiceAuthorized = false
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            break
+        case .authorizedWhenInUse, .authorizedAlways:
+            if CLLocationManager.locationServicesEnabled() {
+                locationServiceAuthorized = true
+                self.locationManager.startUpdatingLocation()
+            }
+        case .restricted, .denied:
+            locationServiceAuthorized = false
+        @unknown default:
+            locationServiceAuthorized = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.currentLocation = locations.last
     }
 }
