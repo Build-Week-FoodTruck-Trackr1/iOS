d8b22ed9b014786f6c573f4cfa7f795142b72f60
Joel Groomer <joelgroomer@gmail.com>
2020-01-12 17:39:02 -0800

    Restoring everything that was lost from the `joel-schedule` branch

--- FoodTruck Trackr/FoodTruck Trackr/Model/ScheduledLocation+Convenience.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ScheduledLocation+Convenience.swift
-//  FoodTruck Trackr
-//
-//  Created by Joel Groomer on 1/9/20.
-//  Copyright © 2020 Julltron. All rights reserved.
-//
-
-import Foundation
