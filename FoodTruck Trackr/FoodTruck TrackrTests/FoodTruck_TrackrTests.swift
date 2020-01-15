f3f4ebc95a137eef628807412c49bf1165a564e9
Joel Groomer <joelgroomer@gmail.com>
2020-01-14 22:56:59 -0800

    added unit tests for fetching food trucks and both successful and failed login

--- FoodTruck Trackr/FoodTruck TrackrTests/FoodTruck_TrackrTests.swift
+++ FoodTruck Trackr/FoodTruck TrackrTests/FoodTruck_TrackrTests.swift
@@ -7,27 +7,55 @@
 //
 
 import XCTest
+@testable import FoodTruck_Trackr
 
 class FoodTruckTrackrTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testApiController() {
+        let apiController = APIController()
+        let expectation = self.expectation(description: "Download food trucks from API")
+        
+        apiController.fetchTrucksFromServer { error in
+            XCTAssertNil(error)
+            XCTAssertFalse(apiController.foodTruckRepresentations.isEmpty)
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 5)
+        
     }
 
+    func testLogin() {
+        
+        /*
+         I believe that my test works, but it will fail. The login function never actually worked correctly.
+         */
+        
+        let apiController = APIController()
+        let expectation = self.expectation(description: "Logged in")
+        
+        let user = User(username: "test123", password: "test", email: "test123@test.com", currentLocation: "test", type: "operator")
+        apiController.LogIn(with: user) { error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(apiController.bearer)
+            expectation.fulfill()
+        }
+        
+        
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testFailedLogin() {
+        let apiController = APIController()
+        let expectation = self.expectation(description: "Not logged in")
+        
+        let user = User(username: "assdzvvdsg", password: "32458", email: "randomasdfasdfag@test.com", currentLocation: "test", type: "operator")
+        apiController.LogIn(with: user) { error in
+            XCTAssertNotNil(error)
+            XCTAssertNil(apiController.bearer)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
 }
